c07a2e92f5a06d0d9a43bf70f68ba3273d0050ff
Aghiles <aghiles.louda@mind-and-go.com>
2023-05-01 18:23:10 +0200

    communication etabli

--- src/main/java/Quizzable/Quiz/Api/model/Question.java
+++ src/main/java/Quizzable/Quiz/Api/model/Question.java
@@ -2,8 +2,11 @@ package Quizzable.Quiz.Api.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -41,10 +44,12 @@ public class Question {
     }
 
     @ManyToOne
+    @JsonIgnore()
     private Categorie categorie;
 
 
     @OneToMany(mappedBy = "question")
+    @JsonIgnore()
     private List<Reponse> reponses;
 
     //@OneToOne
